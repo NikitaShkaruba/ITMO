6788ce44c40ae712c14afbe00b1cd06329c54bc6
Nikita Shkaruba <Sh.Nickita@list.ru>
2015-10-16 16:35:16 +0300

    Add defended Lab2 code

---
+++ Course 2/Software_engineering_basics/Lab2(Git)/Git-repository/G.java
@@ -0,0 +1,34 @@
+public class G extends null {
+
+    String nn();
+
+    int[] ii();
+
+    public java.util.List<String> jj() {
+        return new java.util.ArrayList<String>();
+    }
+
+    public float ff() {
+        return 3.14;
+    }
+
+    public void ab() {
+        return;
+    }
+
+    public byte oo() {
+        return 3;
+    }
+
+    public java.util.Set<Integer> ll() {
+        return new java.util.LinkedList<Integer>;
+    }
+
+    public double ad() {
+        return 11;
+    }
+
+    public void bb() {
+        System.out.println(getClass().getName());
+    }
+}
