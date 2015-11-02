261e2678bb42951f279fc06b822583637c30ae93
Nikita Shkaruba <Sh.Nickita@list.ru>
2015-11-02 11:51:58 +0300

    Add not compiling function code to Lab7

--- Course 2/System_Programming_Languages/Lab7/Lab7.asm
+++ Course 2/System_Programming_Languages/Lab7/Lab7.asm
@@ -17,6 +17,8 @@ includelib c:\masm32\lib\kernel32.lib
 includelib c:\masm32\lib\masm32.lib
 includelib c:\masm32\lib\libc.lib
 
+.stack 100h
+
 .data
 	startMessage db "System Programming Languages, final Lab 7. Made by Shkaruba Nikita, group P3218", 13, 10, 0
 	funcMessage db "My func is:  sin(x + a)", 13, 10, 0
@@ -31,11 +33,13 @@ includelib c:\masm32\lib\libc.lib
 	testA dd 1.6000
 	testPrec dd 0.001
 .data?
-	firstX dq 1 dup(?)
-	deltaX dq 1 dup(?)
-	lastX dq 1 dup(?)
-	a dq 1 dup(?)
-	precision dq 1 dup(?)
+	testResult dd ?
+	firstX dd ?
+	deltaX dd ?
+	lastX dd ?
+	paramA dd ?
+	precision dd ?
+	floatBuffer db 100 ?
 	
 .code
 ;	double computeFunction(double x, double a, double precision) {
@@ -53,18 +57,50 @@ includelib c:\masm32\lib\libc.lib
 ;		
 ;		return sum;
 ;}
-computeFunction proc x:dword, a:dword, precision:dword
-	pushad
-	local temp:dword
+computeFunction proc x:dword, a:dword, prec:dword
+	local i:byte
+	
+	push cx
+	mov i, 0
+	fldz	; sum
+	fldz  ; temp
 	
 	.repeat
-	fld x
+	fst st(0) ; remove temp
+	fld1	; temp = 1
 	
-	.until st(0) > precision
+	lea eax, i
+	mov cl, [eax]
+powMark:
+	fmul x 		; temp *= x^i
+	loop powMark
 	
+	fadd st(1), st(0)
+	fst st(0)
 	
-	popad
-	ret
+	fldpi		; st(0) = pi
+	fild i 	
+	fmul 		; st(0) = pi*i 
+	mov dx, 2
+	fild dx	 
+	fdiv		; st(0) = pi*i/2
+	fadd paramA ; st(0) = pi*i/2 + a
+	fsin 		; st(0) = sin(pi*i/2 + a)
+	fmulp  		; temp *= sin(a + i*Pi/2) 
+	
+	lea eax, i
+	mov cl, [eax]
+faqMark:
+	fdiv cx	; temp /= i!
+	loop faqMark
+	
+	fadd st(1), st(0)	; sum += temp
+	inc i	
+	.until st(0) > prec
+	
+	fst st(0)	; remove temp
+	pop cx
+	ret		; i do not need to pop locals, hehe
 computeFunction endp
 
 
@@ -72,38 +108,43 @@ computeFunctionTest proc
 	pushad
 
 	invoke computeFunction, testX, testA, testPrec
+	fst testResult
 	
 	popad
 	ret
 computeFunctionTest endp
+
+getUserData proc
+	invoke StdOut, addr startMessage
+    invoke StdOut, addr funcMessage
+	
+	invoke StdOut, addr getXmessage
+	invoke StdIn, addr floatBuffer, 100
+	invoke StrToFloat, addr floatBuffer, addr firstX
+	
+	invoke StdOut, addr getDXmessage
+	invoke StdIn, addr floatBuffer, 100
+	invoke StrToFloat, addr floatBuffer, addr deltaX
+	
+	invoke StdOut, addr getFXMessage
+	invoke StdIn, addr floatBuffer, 100
+	invoke StrToFloat, addr floatBuffer, addr lastX
+	
+	invoke StdOut, addr getAMessage
+	invoke StdIn, addr floatBuffer, 100
+	invoke StrToFloat, addr floatBuffer, addr paramA
+	
+	invoke StdOut, addr getPrecisionMessage
+	invoke StdIn, addr floatBuffer, 100
+	invoke StrToFloat, addr floatBuffer, addr precision
+	
+	ret
+getUserData endp
+
+
 start:
-	mov eax, 00FFh
-	xor ax, ax
-	
 	call computeFunctionTest
-	
-	mov eax, 00FFh
-	xor ax, ax
-	
-	;invoke StdOut, addr startMessage
-    ;invoke StdOut, addr funcMessage
-	
-	;invoke StdOut, addr getXmessage
-	;invoke StdIn, addr floatBuffer, 100
-	;invoke StrToFloat, floatBuffer, addr firstx
-	
-	;invoke StdOut, addr getDXmessage
-	;invoke StdIn, addr deltaX, 100
-	
-	;invoke StdOut, addr getFXMessage
-	;invoke StdIn, addr lastX, 100	
-	
-	;invoke StdOut, addr getAMessage
-	;invoke StdIn, addr a, 100
-	
-	;invoke StdOut, addr getPrecisionMessage
-	;invoke StdIn, addr precision, 100
-	
+	call getUserData
 	
 	;invoke computeFunction, startX, a, precision
 	;invoke FloatToStr, result, floatBuffer
