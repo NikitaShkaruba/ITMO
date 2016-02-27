fad5a7d360e1e0f7f414b0053034ea934eb3ad1f
Nikita <sh.nickita@list.ru>
2016-02-28 00:11:09 +0300

    Add Lab1 - cp alternative

--- Course 2/System_software/Lab1/src/copyFunctions.h
+++ Course 2/System_software/Lab1/src/copyFunctions.h
@@ -1,11 +1,9 @@
 #pragma once
 
-#include <bits/stringfwd.h>
-#include <iosfwd>
 #include <iostream>
-#include <vector>
-#include <fstream>
 
 using namespace std;
 
-void cppCopy(string inPath, string outPath);
+void customCopy(string inPath, string outPath);
+void posixCopy(string inPath, string outPath);
+void sendfileCopy(string inPath, string outPath);
--- Course 2/System_software/Lab1/src/main.cpp
+++ Course 2/System_software/Lab1/src/main.cpp
@@ -1,22 +1,7 @@
-#include <iostream>
-#include <sys/sendfile.h>
-#include <fstream>
-#include <vector>
 #include "copyFunctions.h"
+#include <fstream>
 
-ssize_t sendfile(int out_fd, int in_fd, off_t *offset, size_t count);
 using namespace std;
-
-bool isNotEnableToOpenFile(string filename) {
-    ifstream inputStream(filename, ios::in);
-    if (!inputStream.is_open()) {
-        inputStream.close();
-        return true;
-    } else {
-        inputStream.close();
-        return false;
-    }
-}
 
 int main(int argc, char* argv[]) {
     if (argc == 4) {
@@ -24,23 +9,15 @@ int main(int argc, char* argv[]) {
         string inPath = argv[2];
         string outPath = argv[3];
 
-        if (isNotEnableToOpenFile(inPath)) {
-            cout << "Can't open file :(";
-            return 0;
-        }
+        if (key == "--posix" || key == "-x")
+            posixCopy(inPath, outPath);
 
-        // retrieving handles and etc..
-        if (key == "--system" || key == "-s") {
+        if (key == "--language" || key == "-l")
+            customCopy(inPath, outPath);
 
-        }
-        // simple programming way
-        if (key == "--language" || key == "-l") {
-            cppCopy(inPath, outPath);
-        }
-        // alternative to windows's copyFile
-        if (key == "--sendfile") {
+        if (key == "--sendfile" || key == "-s")
+            sendfileCopy(inPath, outPath);
 
-        }
         if (key == "--pretty" || key == "-p") {
             ifstream  src("in.txt", std::ios::binary);
             ofstream  dst("out.txt", std::ios::binary);
@@ -55,11 +32,11 @@ int main(int argc, char* argv[]) {
 
         if (key == "--help" || key == "-h") {
             cout << "::HELP::" << endl;
-            cout << "Usage pattern for dummies: 'Lab1_cp_ --pretty in.txt out.txt'" << endl;
+            cout << "Usage pattern for dummies: 'cp --pretty in.txt out.txt'" << endl;
             cout << "Available keys: " << endl;
-            cout << "\t --system -s" << endl;
+            cout << "\t --posix -x" << endl;
             cout << "\t --language -l" << endl;
-            cout << "\t --sendfile" << endl;
+            cout << "\t --sendfile -s" << endl;
             cout << "\t --pretty -p" << endl;
             return 0;
         }
