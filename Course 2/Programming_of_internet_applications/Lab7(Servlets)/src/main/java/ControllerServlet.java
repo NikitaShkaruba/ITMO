9e2b605cbdb5d4eda70477c59e10ef8bfc9d3354
Nikita <sh.nickita@list.ru>
2016-03-19 16:02:49 +0300

    Add complete lab7 code

--- Course 2/Programming_of_internet_applications/Lab7(Servlets)/src/main/java/ControllerServlet.java
+++ Course 2/Programming_of_internet_applications/Lab7(Servlets)/src/main/java/ControllerServlet.java
@@ -25,9 +25,9 @@ public class ControllerServlet extends HttpServlet {
     }
 
     private boolean isAreaCheck(Map<String, String[]> map) {
-        return (map.get("x") != null && map.get("x").length == 1) && isDouble(map.get("x")[0]) &&
-               (map.get("y") != null && map.get("y").length == 1) && isDouble(map.get("y")[0]) &&
-               (map.get("r") != null && map.get("r").length == 1) && isDouble(map.get("r")[0]);
+        return map.get("x") != null && map.get("x").length == 1 && isDouble(map.get("x")[0]) && Double.valueOf(map.get("x")[0]) >= -3 && Double.valueOf(map.get("x")[0]) <= 4 &&
+               map.get("y") != null && map.get("y").length == 1 && isDouble(map.get("y")[0]) && Double.valueOf(map.get("y")[0]) >= -3 && Double.valueOf(map.get("y")[0]) <= 3 &&
+               map.get("r") != null && map.get("r").length == 1 && isDouble(map.get("r")[0]) && Double.valueOf(map.get("y")[0]) >= 1.0 && Double.valueOf(map.get("y")[0]) <= 3;
     }
     private boolean isDouble(String s) {
         try {
