82ca2856ca0263eb2d28cad423cc86b6f3671bea
Shkaruba Nikita <sh.nickita@list.ru>
2016-10-11 22:38:51 +0300

    Add drop_schema.sql for course_project_1 to dbs

---
+++ Course 3/Database_systems/course_project_1/src/main/sql/drop_schema.sql
@@ -0,0 +1,16 @@
+--- File to drop all the tables
+
+drop table textures;
+drop table cosmetics;
+drop table food;
+drop table entertainments;
+drop table sceneries;
+drop table sceneries;
+drop table user_photoes;
+drop table pets;
+drop table animals;
+drop table users;
+drop table rating;
+drop table user_cosmetic;
+drop table friends;
+drop table master_user;
