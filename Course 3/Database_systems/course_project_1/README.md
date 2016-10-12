b5ff6a8aea91d972330cf69e0490ffe065eaf733
Shkaruba Nikita <snikita@kodeks.ru>
2016-10-12 13:02:54 +0300

    Migrate JAX-RS project to Spring, update README.md

--- Course 3/Database_systems/course_project_1/README.md
+++ Course 3/Database_systems/course_project_1/README.md
@@ -1,25 +1,16 @@
-JAX-RS/Jersey application sample
+Database Systems Course Project 1
 ========================================
-* [JAX-RS API](https://java.net/projects/jax-rs-spec) — Java API for RESTful services
-  * [Jersey](https://jersey.java.net/) — JAX-RS implementation
-* [Gradle](http://www.gradle.org/) — Build Automation tool
-
-On this project, an application server used in a development cycle is:
-
-* [Jetty](http://www.eclipse.org/jetty/) — Servlet engine and HTTP server
-
 
 ### Prerequisites
 
 * [JDK](http://www.oracle.com/technetwork/java/javase/downloads/index.html)
 
-
-### Building and running
+### How to run
 
 ```
-# Building WAR file
-./gradlew --daemon war
+# Run tests
+./gradlew test
 
-# Running on Jetty for development
-./gradlew --daemon jettyRun
+# Run server
+./gradlew bootRun
 ```
---
+++ Course 3/Database_systems/course_project_1/src/main/sql/create_schema.sql
@@ -0,0 +1,91 @@
+--- Script to create all the tables and relations between them
+
+create table textures(
+    id number constraint textures_pk primary key not null,
+    texture blob
+);
+
+create table cosmetics (
+    id number constraint cosmetics_pk primary key not null,
+    name varchar(255)  not null,
+    type varchar(255)  not null,
+    priсe number not null
+);
+
+create table food (
+    id number constraint food_pk primary key not null,
+    name varchar(255)  not null,
+    satiety number not null
+);
+
+create table entertainments(
+    id number constraint entertainments_pk primary key not null,
+    name varchar(255)  not null,
+    quality number
+);
+
+create table sceneries (
+    id number constraint sceneries_pk primary key not null,
+    name varchar(255) not null,
+    texture blob
+);
+
+create table user_photoes (
+    id number constraint user_photoes_pk primary key not null,
+    data blob
+);
+
+create table pets (
+    id number constraint pets_pk primary key not null,
+    user_id number not null,
+    death_date date,
+    happiness number,
+    hungriness number,
+    health number
+);
+
+create table animals (
+    id number constraint animals_pk primary key not null,
+    name varchar(255)  not null,
+    texture_id number not null,
+    CONSTRAINT animals_fk FOREIGN KEY (texture_id) REFERENCES textures(id)
+);
+
+create table users (
+    id number constraint users_pk primary key not null,
+    name varchar(255)  not null,
+    pet_id number not null,
+    photo_id number not null,
+    money_amount number,
+    phone number(11),
+    CONSTRAINT users_fk FOREIGN KEY (pet_id) REFERENCES pets(id),
+    CONSTRAINT users_fk2 FOREIGN KEY (photo_id) REFERENCES user_photoes(id)
+);
+
+ALTER TABLE pets ADD FOREIGN KEY (user_id) REFERENCES users(id);
+
+create table ratings (
+    id number constraint rating_pk primary key not null,
+    user_id number not null,
+    rank number,
+    CONSTRAINT rating_fk FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+create table user_cosmetics (
+    user_id number not null,
+    cosmetic_id number not null,
+    CONSTRAINT user_cosmetic_fk FOREIGN KEY (user_id) REFERENCES users(id),
+    CONSTRAINT user_cosmetic_fk2 FOREIGN KEY (cosmetic_id) REFERENCES cosmetics(id)
+);
+
+create table friends(
+    from_id number not null,
+    to_id number not null,
+    CONSTRAINT friends_fk FOREIGN KEY (from_id) REFERENCES users(id),
+    CONSTRAINT friends_fk2 FOREIGN KEY (to_id) REFERENCES users(id)
+);
+
+create table admin_users (
+    user_id number,
+    CONSTRAINT admin_users_fk FOREIGN KEY (user_id) REFERENCES users(id)
+);
