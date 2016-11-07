60cd112b44cb649970d8580a87623ee9a3885bdf
Shkaruba Nikita <sh.nickita@list.ru>
2016-11-07 16:21:25 +0300

    Add '/api/entertainments' route, moved copy_to_jovian, so we can copy all src

---
+++ course_3/database_systems/course_project_2/src/copy_to_jovian.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+scp -r ./* sigma@146.185.143.190:/home/sigma/code/ITMO/course_3/database_systems/course_project_2/src
+scp -r ./* justPeony@146.185.143.190:/home/sigma/code/ITMO/course_3/database_systems/course_project_2
---
+++ course_3/database_systems/course_project_2/src/main/java/itmo/dbs/models/Entertainment.java
@@ -0,0 +1,17 @@
+package itmo.dbs.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "entertainments")
+public class Entertainment {
+    @Id public String id;
+    public String name;
+    public int quality;
+
+    public Entertainment() {}
+    public Entertainment(String name, int quality) {
+        this.name = name;
+        this.quality = quality;
+    }
+}
