75e6d18d7179160fb59028cee696010bd2310e7e
Никита Шкаруба < >
2016-11-27 13:32:42 -0800

    Refactor Lab4 code

--- course_3/windows_interfaces/lab_4(graphics)/src/Lab4.cpp
+++ course_3/windows_interfaces/lab_4(graphics)/src/Lab4.cpp
@@ -10,15 +10,14 @@
 LONG WINAPI handleWindowEvents(HWND, UINT, WPARAM,LPARAM);
 
 // Just global variabe to be accessedfrom everywhere - It's not my code :)
-HINSTANCE hInst;
-HANDLE hThr;
-WINDOWINFO hInfo;
-COLORREF currentColor = RGB(204, 0, 0);
-HPEN pen = CreatePen(PS_SOLID, 1, currentColor), oldPen;
-HBRUSH brush = CreateSolidBrush(currentColor), oldBrush;
-POINT points[4];
-int offset = 0, w, h, direction = 1;
-int timerDelay = 100;
+COLORREF currentColor = RGB(10, 10, 10);
+HBRUSH brush = CreateSolidBrush(currentColor);
+int windowWidth, windowHeight;
+
+// Figure
+int figureDirection = 1, figureScale = 10, figureStep = 1;
+const int figurePointsLength = 16; 
+POINT figurePoints[figurePointsLength];
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     HWND hWnd;
@@ -28,7 +27,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
     windowClass.style = WS_TILED | CS_HREDRAW | CS_VREDRAW;
     windowClass.lpfnWndProc = handleWindowEvents;
-    windowClass.hInstance = hInst = hInstance;
+    windowClass.hInstance = hInstance;
 	windowClass.hbrBackground = (HBRUSH)GetStockObject(LTGRAY_BRUSH);
     windowClass.lpszClassName = L"Lab4";
 	windowClass.hIcon = LoadIcon(NULL, IDI_EXCLAMATION);
@@ -36,7 +35,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     
 	RegisterClass(&windowClass);
 	hWnd = CreateWindow(L"Lab4", L"Main Window", WS_POPUP, 0, 0, GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN), NULL, NULL, hInstance, NULL);
-	SetTimer(hWnd, 1, timerDelay, (TIMERPROC) NULL);
+	SetTimer(hWnd, 1, 20, (TIMERPROC) NULL);
 	
 	ShowWindow(hWnd, SW_SHOWMAXIMIZED);
 	UpdateWindow(hWnd);
@@ -49,22 +48,70 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	return message.wParam;
 }
 
-void PaintPolygon(HDC hdc) {
-	SelectObject(hdc, pen);
-	SelectObject(hdc, brush);
+void initializeFigure() {	
+	// Bottom right two spikes
+	figurePoints[0].x =  + figureScale*(0);
+	figurePoints[0].y = windowHeight/2 + figureScale*(1);
 
-	points[0].x = w/2+15+offset;
-	points[1].x = w/2+35+offset;
-	points[2].x = w/2-15+offset;
-	points[3].x = w/2-35+offset;
-	points[0].y = h/2+10;
-	points[1].y = h/2-10;
-	points[2].y = h/2-10;
-	points[3].y = h/2+10;
+	figurePoints[1].x = figureScale*(1);
+	figurePoints[1].y = windowHeight/2 + figureScale*(3);
 
-	Polygon(hdc, points, 4);
+	figurePoints[2].x = figureScale*(0.8);
+    figurePoints[2].y = windowHeight/2 + figureScale*(0.8);
+
+	figurePoints[3].x = figureScale*(3);
+	figurePoints[3].y = windowHeight/2 + figureScale*(1);
+
+	figurePoints[4].x = figureScale*(1);
+	figurePoints[4].y = windowHeight/2 + figureScale*(0);	
+
+	// Top right two spikes
+	figurePoints[5].x = figureScale*(3);
+	figurePoints[5].y = windowHeight/2 + figureScale*(-1);
+	
+	figurePoints[6].x = figureScale*(0.8);
+	figurePoints[6].y = windowHeight/2 + figureScale*(-0.8);
+	
+	figurePoints[7].x = figureScale*(1);
+	figurePoints[7].y = windowHeight/2 + figureScale*(-3);
+	
+	figurePoints[8].x = figureScale*(0);
+	figurePoints[8].y = windowHeight/2 + figureScale*(-1);
+
+	// Top left two spikes
+	figurePoints[9].x = figureScale*(-1);
+	figurePoints[9].y = windowHeight/2 + figureScale*(-3);
+
+	figurePoints[10].x = figureScale*(-0.8);
+    figurePoints[10].y = windowHeight/2 + figureScale*(-0.8);
+
+	figurePoints[11].x = figureScale*(-3);
+	figurePoints[11].y = windowHeight/2 + figureScale*(-1);
+
+	figurePoints[12].x = figureScale*(-1);
+	figurePoints[12].y = windowHeight/2 + figureScale*(0);	
+
+	// Bottom left two spikes
+	figurePoints[13].x = figureScale*(-3);
+	figurePoints[13].y = windowHeight/2 + figureScale*(1);
+	
+	figurePoints[14].x = figureScale*(-0.8);
+	figurePoints[14].y = windowHeight/2 + figureScale*(0.8);
+	
+	figurePoints[15].x = figureScale*(-1);
+	figurePoints[15].y = windowHeight/2 + figureScale*(3);
+}
+void updateFigure() {
+	// Bottom right two spikes
+	for(int i = 0; i < figurePointsLength; i++) {
+		figurePoints[i].x += figureStep * figureDirection;;
+	}
 }
 
+void PaintPolygon(HDC hdc) {
+	SelectObject(hdc, brush);
+	Polygon(hdc, figurePoints, 16);
+}
 void RepaintWindow(HWND hWnd) {
 	InvalidateRect(hWnd, NULL, TRUE);
 	UpdateWindow(hWnd);
@@ -73,8 +120,8 @@ void RepaintWindow(HWND hWnd) {
 LONG WINAPI handleWindowEvents(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
 	switch (message) {
 	case WM_PAINT:
-		w = GetSystemMetrics(SM_CXSCREEN);
-		h = GetSystemMetrics(SM_CYSCREEN);
+		windowWidth = GetSystemMetrics(SM_CXSCREEN);
+		windowHeight = GetSystemMetrics(SM_CYSCREEN);
 
 		HDC hdc;
 		PAINTSTRUCT ps;
@@ -84,34 +131,27 @@ LONG WINAPI handleWindowEvents(HWND hWnd, UINT message, WPARAM wParam, LPARAM lP
 		EndPaint(hWnd, &ps);
 		break;
 	case WM_TIMER: 
-		if (direction == -1) {
-			if (points[3].x - 10 >= 0) {
-				offset -= 10;
-				RepaintWindow(hWnd);
-			} else {
-				direction = 1;
-			}
+		// Caclulate parameters/
+		if (figurePoints[0].x > windowWidth) {
+			figureDirection = -1;
+		} else if (figurePoints[0].x < 0) {
+			figureDirection = 1;
 		}
-		if (direction == 1){
-			if (points[1].x + 10 <= w){
-				offset += 10;
-				RepaintWindow(hWnd);
-			} else {
-				direction = -1;
-			}
-		}
-		SetTimer(hWnd, 1, timerDelay, (TIMERPROC) NULL); // Reset timer
+
+		updateFigure();
+		RepaintWindow(hWnd);
+		SetTimer(hWnd, 1, 60, (TIMERPROC) NULL); // Reset timer
 		break;
 	case WM_KEYDOWN:
 		if (wParam == VK_UP) {
-			if (timerDelay > 50) {
-				timerDelay -= 10; 
+			if (figureStep < 50) {
+				figureStep += 1;
 			} else {
 				MessageBeep(0);
 			}
 		} else if (wParam == VK_DOWN) {
-			if (timerDelay < 10000) {
-				timerDelay += 10; 
+			if (figureStep > 0) {
+				figureStep -= 1;
 			} else {
 				MessageBeep(0);
 			}
@@ -125,17 +165,9 @@ LONG WINAPI handleWindowEvents(HWND hWnd, UINT message, WPARAM wParam, LPARAM lP
 			exit(0);
 		break;
 	case WM_CREATE:
-		w = GetSystemMetrics(SM_CXSCREEN);
-		h = GetSystemMetrics(SM_CYSCREEN);
-
-		points[0].x = w/2+15+offset;
-		points[1].x = w/2+35+offset;
-		points[2].x = w/2-15+offset;
-		points[3].x = w/2-35+offset;
-		points[0].y = h/2+10;
-		points[1].y = h/2-10;
-		points[2].y = h/2-10;
-		points[3].y = h/2+10;
+		windowWidth = GetSystemMetrics(SM_CXSCREEN);
+		windowHeight = GetSystemMetrics(SM_CYSCREEN);
+		initializeFigure();
 		break;
 	default:
 		return DefWindowProc(hWnd, message, wParam, lParam);
