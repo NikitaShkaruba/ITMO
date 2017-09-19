e53c7a41318a39f00b0b52877280d1c6f236fa42
Sergey <gray4747@mail.ru>
2017-09-19 17:12:17 +0300

    Table without triggers

--- course_4/database_managment_systems/lab_1/create_schema.sql
+++ course_4/database_managment_systems/lab_1/create_schema.sql
@@ -1,5 +1,7 @@
 create table trigger_test (id integer);
 
+create table triggerless_test (col1 integer, col2 integer);
+
 create or replace trigger trigger_test_insert
 before insert on trigger_test for each row
 begin
